f0cb914a734751e951c05fa10dcbb526e0a228a5
Mohamed Merdan <merdan96@yahoo.com>
2016-08-07 21:25:35 +0200

    created interfaces and finished movie search ui

---
+++ app/src/main/java/merdan/com/androidproject/Main.java
@@ -0,0 +1,15 @@
+package merdan.com.androidproject;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ app/src/main/java/merdan/com/androidproject/MovieView.java
@@ -0,0 +1,15 @@
+package merdan.com.androidproject;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class MovieView extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movieView);
+    }
+
+}
